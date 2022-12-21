7745d61de7b27606933ca2dee37a6262dcc6390c
Markkimotho <martinozas@gmail.com>
2022-12-21 19:33:52 +0000

    all mandatory tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size
