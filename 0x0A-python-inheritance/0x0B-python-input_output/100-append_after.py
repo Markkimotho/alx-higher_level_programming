0310f65c0142c989d67008793a22bcb017e489b0
Markkimotho <martinozas@gmail.com>
2023-01-10 21:05:29 +0000

    secondary commit

---
+++ 0x0A-python-inheritance/0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module that defines a text file insertion function"""
+
+def append_after(filename="", search_string="", new_string=""):
+    """The function inserts text after each line containing a given string in a file
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
