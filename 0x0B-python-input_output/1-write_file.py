6e13e9e56fb71a340c118b5fc71135f6d0b438d9
Markkimotho <martinozas@gmail.com>
2023-01-11 19:35:28 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a file-writing function."""
 
-"""Module that writes a string to a text file (UTF8)
-	and returns the number of characters written
-"""
 
 def write_file(filename="", text=""):
-	
-	"""
-	Function that writes a string to a UTF-8 unicode file
-	"""
-	with open(filename, 'w', encoding="utf-8") as f:
-		print(f.read(), end="")
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
