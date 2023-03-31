c8a1c64bbe838505054abd6e4122cf3e0d5a3c76
MarkTinega <martinozas@gmail.com>
2023-03-31 20:35:05 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(url_content)))
         print("\t- content: {}".format(url_content))
-        print("\t- uf8 content: {}".format(url_content.decode("utf-8")
+        print("\t- uf8 content: {}".format(url_content.decode("utf-8"))
