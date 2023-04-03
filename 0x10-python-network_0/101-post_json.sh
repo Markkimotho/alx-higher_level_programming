960bf23ca6c3c44e8e578a53c0e9845d5edb91ab
Markkimotho <martinozas@gmail.com>
2023-04-03 22:35:33 +0300

    checking the word count on answer files

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-# Your script must send a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request
-
-# read the contents of the file into a variable
-file_data=$(cat "$2")
-
-# Send the POST request using curl and display the response body
-curl -s -X POST -H "Content-Type: application/json" -d "$file_data" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-# You are not allow to use echo, cat, etc. to display the final result
-
 curl -s -L -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
